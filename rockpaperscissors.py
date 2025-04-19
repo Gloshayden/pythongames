3ea833e84ea5eb11589910b0988666757f598c95
Gloshayden <121631406+Gloshayden@users.noreply.github.com>
2025-04-19 19:36:23 +0100

    Update rockpaperscissors.py

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -5,33 +5,24 @@ rps = ['rock', 'paper', 'scissors']
 creds = 20
 debug = False
 print("welcome to rock paper scissors")
-while creds > 0 and debug == False:
-    x = random.choice(rps)
-    print("you have", creds,"credits")
-    print("what will you choose rock paper or scissors")
-    y = str(input())
-    if x == y:
-        creds = creds + 1
-        print("well done you won +1 credits")
-        time.sleep(2)
-        print(chr(27) + "[2J")
-    elif x != y:
-        creds = creds - 1
-        print("you lost -1 credits")
-        time.sleep(2)
-        print(chr(27) + "[2J")
+while creds > 0:
+  if debug == True: creds = 999999999
+  x = random.choice(rps)
+  print(f"you have {creds} credits")
+  print("what will you choose rock paper or scissors")
+  if debug == True: print(f"Answer: {x}")
+  y = str(input())
+  y = y.lower()
+  if x == y:
+    creds = creds + 1
+    print("well done you won +1 credits")
+    time.sleep(2)
+    print("")
+  elif x != y:
+    creds = creds - 1
+    print("you lost -1 credits")
+    time.sleep(2)
+    print("")
 while creds <= 0:
     print("you ran out of credits, thank you for playing")
-    time.sleep(120)
-while creds > 0 and debug == True:
-    x = random.choice(rps)
-    creds = 999999999999
-    print("Debug mode enabled, Answer revieled and unlimited creds")
-    print("Answer:", x)
-    y = str(input())
-    if x == y:
-        creds = creds + 1
-        print("well done you won +1 credits")
-    elif x != y:
-        creds = creds - 1
-        print("you lost -1 credits")
+    time.sleep(10)

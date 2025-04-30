e73e2f0b7989e864c38d5f81626a758ca93d9713
Gloshayden <Gloshayden@gmail.com>
2025-04-30 19:22:06 +0100

    created a first sime startup

--- hangman.py
+++ hangman.py
@@ -1,4 +1,4 @@
-import random, json
+import random, json, os
 
 def getWord():
     print("Loading words...")
@@ -8,7 +8,17 @@ def getWord():
     test = words[random.randint(0,len(words)-1)]
     return test
 
+# first time startup
+if not os.path.exists("words.json"):
+    print("No saved words found. Generating file")
+    with open("words.json", "w") as f:
+        structure = {"words":["enter", "words", "here"]}
+        json.dump(structure, f)
+    print("Made words file please enter in words there. \n" \
+    "Or you can use the premade file with the words: 'enter','words','here'")
+    input("Please edit the file then come back and press enter.")
+
 word = getWord()
-print("Welcome to hangman! \n Whats your name?")
+print("Welcome to hangman! \nWhats your name?")
 name = input("Enter name here: ")
 print(f"Hello {name} lets get started\n")
